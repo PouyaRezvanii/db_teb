a3aeef3c52dcad50bcec535c8e5baf71172e5f87
pouya <pouyarezvani79@gmail.com>
2024-11-04 18:00:04 +0330

    initial commit and create Product model

---
+++ src/models/product.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    productUrl: {
+        type: String,
+        required: true
+    },
+
+},
+{
+    timestamps :{
+        createdAt: "created_at",
+        updatedAt: "updated_at"
+    }
+})
+
+module.exports = mongoose.model('Product', productSchema);
